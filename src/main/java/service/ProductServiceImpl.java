b496a7ad4fd9a810bbfd29260757c2a07b6be68e
marwandi <marwandi@Integrasi-MacBook-Pro.local>
2019-07-04 22:28:30 +0700

    setting impl

--- src/main/java/service/ProductServiceImpl.java
+++ src/main/java/service/ProductServiceImpl.java
@@ -1,4 +1,36 @@
 package service;
 
-public class ProductServiceImpl {
+import entity.ProductEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.ProductRepository;
+
+@Service("ProductService")
+public class ProductServiceImpl implements ProductService {
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Iterable<ProductEntity> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public ProductEntity getProductEntityById(Integer id) {
+        return productRepository.findOne(id);
+    }
+
+    @Override
+    public ProductEntity saveProductEntity(ProductEntity productEntity) {
+        return productRepository.save(productEntity);
+    }
+
+    @Override
+    public void deleteProductEntity(Integer id) {
+        productRepository.delete(id);
+    }
 }
