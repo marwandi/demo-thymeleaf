88cdb278f1b83f0bac4ebafd9ed0dca29e5b02e2
marwandi <marwandi@Integrasi-MacBook-Pro.local>
2019-07-04 21:19:36 +0700

    pertama kali

---
+++ src/main/java/com/warlock1/learn/demowarlockthymeleaf/DemoWarlockThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.warlock1.learn.demowarlockthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWarlockThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWarlockThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warlock1/learn/demowarlockthymeleaf/DemoWarlockThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package com.warlock1.learn.demowarlockthymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoWarlockThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
